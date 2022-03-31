ca190ef0e1fbfe3ada8f2c83982f2993eb5b9fe9
jeffreyally <jeffreyally@gmail.com>
2022-03-31 21:12:25 +0000

    working on todos with back end

--- src/js/component/taskswithdelete.jsx
+++ src/js/component/taskswithdelete.jsx
@@ -1,6 +1,31 @@
 import React from "react";
 import { useState } from "react";
 import { Todolist } from "./todolist.jsx";
+let URL = `https://3000-jeffreyally-tasklistwith-2becw0mjhk0.ws-us38.gitpod.io/tasks`;
+let DeleteTask = (modifyTask, id) => {
+	fetch(`${URL}/${id}`, {
+		method: "DELETE",
+		body: JSON.stringify(), // data can be `string` or {object}!
+		headers: {
+			"Content-Type": "application/json",
+		},
+	})
+		.then((resp) => {
+			return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+		})
+		.then((data) => {
+			// shout out to Christina for the
+			//here is were your code should start after the fetch finishes
+			console.log(data); //this will print on the console the exact object received from the server
+			modifyTask(data);
+			return data;
+		})
+		.catch((error) => {
+			//error handling
+			console.log(error);
+			//from class notes ----console.error("error", error);
+		});
+};
 
 export const TasksWithDeleteIcon = (props) => {
 	return (
@@ -16,24 +41,29 @@ export const TasksWithDeleteIcon = (props) => {
 										<span
 											className="thetask"
 											id={makespanIDtheindex}>
-											{maptask.label}
+											{maptask.task}
 
 											<i
 												className="fa fa-trash"
 												onClick={(e) =>
 													props.modlist(
-														props.tlist.filter(
-															(
-																eachtask,
-																eachtaskindex
-															) => {
-																//this is where I think I will delete one task from the todo list
-																//(PUT request?)
-																return (
-																	eachtaskindex !=
-																	makespanIDtheindex
-																);
-															}
+														// props.tlist.filter(
+														// 	(
+														// 		eachtask,
+														// 		eachtaskindex
+														// 	) => {
+														// 		//this is where I think I will delete one task from the todo list
+														// 		//(PUT request?)
+														// 		return (
+														// 			eachtaskindex !=
+														// 			makespanIDtheindex
+														// 		);
+														// 	}
+														// )
+														DeleteTask(
+															props.modlist,
+															maptask.id,
+															props.URL
 														)
 													)
 												}></i>
--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -1,6 +1,56 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { TasksWithDeleteIcon } from "./taskswithdelete.jsx";
+
+let URL = `https://3000-jeffreyally-tasklistwith-2becw0mjhk0.ws-us38.gitpod.io/tasks`;
+
+let GetDB = (modifyTask) => {
+	fetch(URL, {
+		method: "GET",
+		body: JSON.stringify(), // data can be `string` or {object}!
+		headers: {
+			"Content-Type": "application/json",
+		},
+	})
+		.then((resp) => {
+			return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+		})
+		.then((data) => {
+			// shout out to Christina for the
+			//here is were your code should start after the fetch finishes
+			console.log(data); //this will print on the console the exact object received from the server
+			modifyTask(data);
+		})
+		.catch((error) => {
+			//error handling
+			console.log(error);
+			//from class notes ----console.error("error", error);
+		});
+};
+
+let PostToDB = (inputTask, modifyTask) => {
+	fetch(URL, {
+		method: "POST",
+		body: JSON.stringify({ task: inputTask, label: false }), // data can be `string` or {object}!
+		headers: {
+			"Content-Type": "application/json",
+		},
+	})
+		.then((resp) => {
+			return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+		})
+		.then((data) => {
+			// shout out to Christina for the
+			//here is were your code should start after the fetch finishes
+			console.log(data); //this will print on the console the exact object received from the server
+			modifyTask(data);
+		})
+		.catch((error) => {
+			//error handling
+			console.log(error);
+			//from class notes ----console.error("error", error);
+		});
+};
 
 //const URI = 'http://assets.breatheco.de/apis/fake/todos/user/jeffreyally'
 //useEffect(() => {
@@ -10,20 +60,26 @@ import { TasksWithDeleteIcon } from "./taskswithdelete.jsx";
 export const TodoList = (props) => {
 	const [inputTask, setInputTask] = useState("");
 	const [taskList, modifyTask] = useState([]);
+	useEffect(() => {
+		//Runs only on the first render
+		GetDB(modifyTask);
+	}, []);
+	useEffect(() => {
+		//Runs on the first render
+		//And any time any dependency value changes
+	}, [taskList]);
 	//update modifyTask to setTaskList in the future
 	let saveTask = (e) => {
 		if (e.keyCode == 13) {
-			// 	const newTask = {
-			// 		task: inputTask,
-			// 		done: false,
-			// 	}
-			modifyTask([...taskList, { label: inputTask, done: false }]);
+			PostToDB(inputTask, modifyTask);
+			// hold up modifyTask([...taskList, { task: inputTask, label: false }]);
 			//above modifyTask([...taskList, newTask]) could also be used
 			//this is where I think I will send a task to the backend(PUT request?)
 			//it needs to be an array of objects so I believe that part is in place at least
 			setInputTask("");
 		}
 	};
+
 	return (
 		<>
 			<div className="todolistbox">
@@ -35,7 +91,11 @@ export const TodoList = (props) => {
 						value={inputTask}
 						onChange={(e) => setInputTask(e.target.value)}></input>
 				</div>
-				<TasksWithDeleteIcon tlist={taskList} modlist={modifyTask} />
+				<TasksWithDeleteIcon
+					tlist={taskList}
+					modlist={modifyTask}
+					URL={URL}
+				/>
 			</div>
 		</>
 	);
